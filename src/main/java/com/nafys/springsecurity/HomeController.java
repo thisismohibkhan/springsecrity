8b28438007df18719b9a30c402b39a42ae580731
Mohib Khan <thisismohib@gmail.com>
2019-07-14 11:31:31 +0530

    First commit

---
+++ src/main/java/com/nafys/springsecurity/HomeController.java
@@ -0,0 +1,24 @@
+package com.nafys.springsecurity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "home.jsp";
+	}
+	
+	
+	@RequestMapping("/login")
+	public String loginPage() {
+		return "login.jsp";
+	}
+	
+	@RequestMapping("/logout-success")
+	public String logoutPage() {
+		return "logout.jsp";
+	}
+}
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Spring Security</title>
+</head>
+<body>
+	Welcome to Spring security... 
+	
+	<br><br>
+	<a href="/logout">Logout</a>
+</body>
+</html>
